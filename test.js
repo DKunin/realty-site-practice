2348d684050ad1615a93a5ca1a352e9a36711f69
Dmitry Kunin <dkunin@avito.ru>
2017-06-15 16:53:02 +0300

    master:Added testing scaffold

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import test from 'ava';
+import initialState from './src/app/redux/store/initialstate';
+
+console.log(initialState());
+
+test('', async t => {
+    t.pass();
+});
